6e708e9f34c517dba46c8cd3065fd726546268dc
Kevin Trautvetter <30321316+glaciyan@users.noreply.github.com>
2025-02-21 21:47:51 +0100

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,15 @@ import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: vercel()
 });
